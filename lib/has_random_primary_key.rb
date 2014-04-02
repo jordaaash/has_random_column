b587b76cfd4271cc0d7d4c1a1d636e1ddf657d5d
Jordan Sexton <jordan@jordansexton.com>
2014-04-02 02:51:33 -0500

    Simplifying module structure to allow shorter require statements

---
+++ lib/has_random_primary_key.rb
@@ -0,0 +1,18 @@
+require 'has_random_column'
+require 'has_random_integer'
+require 'securerandom'
+
+module HasRandomColumn
+  def has_random_primary_key (options = {}, &block)
+    options = {
+      :column => nil,
+      :unique => true
+    }.merge!(options)
+    column  = options.delete(:column) || primary_key
+    unless block_given?
+      # + 1 since 0 <= SecureRandom.random_number(n) < n may = 0
+      block = Proc.new { |maximum| SecureRandom.random_number(maximum) + 1 }
+    end
+    has_random_integer(column, options, &block)
+  end
+end
